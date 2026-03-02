e2a89a93a0162255e819757be324850dfcbf2541
JsHwang0916 <junsung.hwang@stonybrook.edu>
2026-03-02 15:15:27 -0500

    Github pages setting changes

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function AppHeader() {
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <AppHeader />
         <Routes>
