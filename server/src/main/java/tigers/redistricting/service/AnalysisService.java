a71b56c6acfebb6067d2692bb7e2ce6d6c03191d
kevdarby <Darbykevin2@gmail.com>
2026-03-12 18:30:46 -0400

    final

--- server/src/main/java/tigers/redistricting/service/AnalysisService.java
+++ server/src/main/java/tigers/redistricting/service/AnalysisService.java
@@ -77,6 +77,6 @@ public class AnalysisService {
     private double[] fitPolynomial(double[] xs, double[] ys) {
         WeightedObservedPoints points = new WeightedObservedPoints();
         for (int i = 0; i < xs.length; i++) points.add(xs[i], ys[i]);
-        return PolynomialCurveFitter.create(POLY_DEGREE).fit(points.toList());
+        return PolynomialCurveFitter.create(3).fit(points.toList());
     }
 }
