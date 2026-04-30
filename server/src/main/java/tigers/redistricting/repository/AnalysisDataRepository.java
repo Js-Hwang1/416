dfd8dcccd1af7b7f5f4940bf102f07ea51163c69
kevdarby <Darbykevin2@gmail.com>
2026-04-30 18:51:43 -0400

    cachable db

--- server/src/main/java/tigers/redistricting/repository/AnalysisDataRepository.java
+++ server/src/main/java/tigers/redistricting/repository/AnalysisDataRepository.java
@@ -1,8 +1,13 @@
 package tigers.redistricting.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import tigers.redistricting.enums.StateId;
 import tigers.redistricting.model.AnalysisData;
 
+import java.util.Optional;
+
 public interface AnalysisDataRepository extends MongoRepository<AnalysisData, StateId> {
+    @Cacheable("analysisData")
+    Optional<AnalysisData> findById(StateId id);
 }
